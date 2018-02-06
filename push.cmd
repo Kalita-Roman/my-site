74af563949a3e81e6e65871932bbe1eb891d637a
Roman Kalita <z11z22z33@mail.ru>
2018-02-06 21:06:30 +0300

    try

--- push.cmd
+++ push.cmd
@@ -1,6 +1,7 @@
 git add .
 git commit -m "try"
-SET NODE_ENV=production&&webpack --config webpack/webpack.config.js
+SET NODE_ENV=production
+webpack --config webpack/webpack.config.js
 git add .
 git commit -m "try build"
 git push origin master
